f6521d416b289f84f4e69950edddf01d1ca2b7ae
nebrelbug <nebrelbug@gmail.com>
2017-10-13 20:58:19 -0600

    fixed fse

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var fs = require('fs') // this engine requires the fs module
 exports.__express = function(filePath, options, callback) {
-  fse.readFile(filePath, function (err, content) {
+  fs.readFile(filePath, function (err, content) {
     if (err) return callback(err)
     // this is an extremely simple template engine
     var rendered = content.toString();
