a709014f338cf2b097360197ee4f400aca1c8378
nebrelbug <nebrelbug@gmail.com>
2018-07-10 09:11:20 +0100

    Finally, everything kinda' working!

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 const compiler = require('google-closure-compiler-js').gulp();
 
 gulp.task('compress', function() {
-  return gulp.src('./precomp2.js', {base: './'})
+  return gulp.src('./squirrelly.js', {base: './'})
       // your other steps here
       .pipe(compiler({
           compilationLevel: 'SIMPLE',
-          jsOutputFile: 'precomp.min.js',  // outputs single file
+          jsOutputFile: 'squirrelly.min.js',  // outputs single file
         }))
-      .pipe(gulp.dest('./dist'));
+      .pipe(gulp.dest('./'));
 });
