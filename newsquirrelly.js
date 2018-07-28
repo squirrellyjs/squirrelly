f6e244bb070c8f67e1402d51fdd8d07ebad7d5cf
nebrelbug <nebrelbug@gmail.com>
2018-07-27 22:04:10 -0600

    I'm trying to make Squirrelly even faster, have id's be optional

--- newsquirrelly.js
+++ newsquirrelly.js
@@ -46,7 +46,7 @@
         Sqrl.Helpers[name] = callback
         Sqrl.H = Sqrl.Helpers
     }
-    /*Sqrl.Str = function (thing) {  To make it more safe...I'll probably have people opt in for performance though. An idea to avoid crashes.
+    Sqrl.Str = function (thing) { /* To make it more safe...I'll probably have people opt in for performance though */
         if (typeof thing === 'string') {
             return thing
         } else if (typeof thing === 'object') {
@@ -54,7 +54,7 @@
         } else {
             return thing.toString()
         }
-    }*/
+    }
 
     Sqrl.Render = function (template, options) {
         return template(options, Sqrl)
@@ -135,8 +135,8 @@
     Sqrl.Compiler.RegExps = {
         /* These are the default RegExps, when the tag isn't changed */
         helperRef: /{{\s*@(?:([\w$]*):)?\s*(.+?)\s*((?: *?\| *?[\w$]* *?)* *?\|*)}}/g, // Helper Reference (with a @)
-        globalRef: /{{\s*?([^#@.\(\\/]+?(?:[.[].*?)*?)((?: *?\| *?[\w$]* *?)* *?\|*)}}/g, // Global reference (No prefix), supports filters
-        helper: /{{ *?([\w$]+) *?\(([^\n]*)\) *?([\w$]*) *?}}([^]*?)((?:{{ *?# *?([\w$]*) *?}}[^]*{{ *?\/ *?\6 *?}}\s*)*){{ *?\/ *?\1 *? \3 *?}}/g, // Helper
+        globalRef: /{{\s*?([^#@.\(\\/ ]+?(?:[.[].*?)*?)((?: *?\| *?[\w$]* *?)* *?\|*)}}/g, // Global reference (No prefix), supports filters
+        helper: /{{ *?([\w$]+) *?\(([^\n]*)\)((?: *?\| *?[\w$]* *?)* *?\|*) *?([\w$]*) *?}}([^]*?)((?:{{ *?# *?([\w$]*) *?}}[^]*{{ *?\/ *?\7 *?}}\s*)*){{ *?\/ *?\1 *? \4 *?}}/g, // Helper
         helperBlock: /{{ *?# *?(\w*) *?}}([^]*){{ *?\/ *?\1 *?}}(?:\s*{{!--[\w$\s]*--}}\s*)*/g, // Helper block
         comment: /{{!--[^]*?--}}/g, // Comment regexp
         parameterGlobalRef: /~~/g, // Parameter is a Global ref
@@ -144,77 +144,10 @@
         partial: /{{ *?(?:>|include) *?([\w$]*)((?: *?\| *?[\w$]* *?)* *?\|*)}}/g // To, obviously, get partials. Can be like {{>partial}} or {{include partial}}. p1 partial name, p2 filters
     }
 
-    /*Sqrl.Compiler.SetTags = function (otag, ctag) {
-        var newRegExps = {}
-        return newRegExps
-    }*/
-
     /* IMPORTANT PARSING FUNCTIONS */
     /* To separate all non-helper blocks of text into global refs and not global refs. I'll probably make this more efficient sometime... */
     /* To parse the string into blocks: helpers and not helpers, after which it'll get parsed into refs and strings */
-    function parseString(strng, varName, regexps) {
-        regexps = regexps || Sqrl.Compiler.RegExps
-        var funcString = 'var ' + varName + '="";'
-        var str = strng.replace(regexps.comment, '')
-        var lastMatchIndex = 0
-        str.replace(regexps.helper, function (m, p1, p2, p3, p4, p5, p6, offset) {
-            // p1 is helper name, p2 is helper parameters, p3 helper id, p4 helper first block, p5 everything else inside
-            var name = p1 || ''
-            var args = p2 || ''
-            var id = p3 || ''
-            var firstblock = p4 || ''
-            var content = p5 || ''
-            funcString = parseGlobalRefs(str.slice(lastMatchIndex, offset), varName, funcString, regexps)
-            lastMatchIndex = offset + m.length
-            var helperFuncName = name + id
-            // The following 2 "replace"s are for converting args into array form, converting all helper refs and global refs into options.*, helpervals.*, etc
-            var params = args.replace(regexps.parameterGlobalRef, 'options.')
-                .replace(regexps.parameterHelperRef, function (mp, pp1, pp2) { // p1 scope, p2 string
-                    if (typeof pp2 === 'undefined') {
-                        return mp
-                    } else {
-                        if (typeof pp1 === 'undefined') {
-                            pp1 = ''
-                        }
-                        return 'helpervals' + pp1 + '.' + pp2
-                    }
-                })
 
-            function createBlockFunction(fname, fid, fcontent) {
-                var returnFunc = 'function (helpervals) {'
-                returnFunc += 'var helpervals' + fid + '=helpervals;' + parseString(fcontent || '', 'block' + fname + fid) + '}'
-                return returnFunc
-            }
-
-            function returnBlockFunctions(bId, bFirstblock, bContent) {
-                var blockFunctions = {}
-                blockFunctions.default = createBlockFunction('default', bId, bFirstblock)
-                bContent.replace(regexps.helperBlock, function (m2, mp1, mp2) {
-                    var bFunc = createBlockFunction(mp1, bId, mp2).replace(/\n/g, '\\n').replace(/\r/g, '\\r')
-                    blockFunctions[mp1] = bFunc
-                })
-                return blockFunctions
-            }
-            
-            if (Sqrl.builtInHelpers.hasOwnProperty(name)) {
-                var blockFuncs = returnBlockFunctions(id, firstblock, content)
-                funcString += Sqrl.builtInHelpers[name](params, blockFuncs, varName, regexps)
-            } else {
-                funcString += varName + '+=Sqrl.H.' + name + '([' + params + '], ' + createBlockFunction('default', id, firstblock) + ', {'
-                content.replace(regexps.helperBlock, function (m2, mp1, mp2) {
-                    funcString += mp1 + ':' + createBlockFunction(mp1, id, mp2) + ','
-                })
-                funcString += '}, options);'
-            }
-            lastMatchIndex = offset + m.length
-        })
-
-        if (str.length > lastMatchIndex) {
-            funcString = parseGlobalRefs(str.slice(lastMatchIndex, str.length), varName, funcString, regexps)
-        }
-        funcString += 'return ' + varName
-        return funcString
-    }
 
     function returnFiltered(filterString, initialString) {
         var filtersArray;
@@ -295,7 +228,37 @@
 
     Sqrl.Precompile = function (str) {
         var regexps = Sqrl.Compiler.RegExps
-        var funcString = parseString(str, 'tRes', regexps)
+        console.log("regexps: " + JSON.stringify(regexps))
+        var regEx = /{{ *?(?:(?:([\w$]+) *?\(([^\n]*?)\)((?: *?\| *?[\w$]* *?)* *?\|*) *?([\w$]*))|(?:\/ *?([\w$]+))) *?}}/g;
+        var lastIndex = 0
+        var oLength = -1;
+        var funcStr = ""
+        var outstanding = [];
+        var helperResult = ""
+        currentVarName = "tmpltRes"
+        str.replace(regEx, function (m, p1, p2, p3, p4, p5, offset) {
+            if (p1 === null || typeof p1 === "undefined") {
+                var previous = outstanding[oLength];
+                if (previous && previous === p5) {
+                    console.log("lastIndex --> cTag: " + str.slice(lastIndex, offset))
+                    lastIndex = offset + m.length
+                    //outstanding.pop(); don't actually need this
+                    oLength -= 1;
+                }
+            } else {
+                oLength += 1;
+                console.log("lastIndex --> oTag: " + str.slice(lastIndex, offset))
+                lastIndex = offset + m.length
+
+                outstanding[oLength] = p1;
+                //console.log("outstanding: " + JSON.stringify(outstanding))
+            }
+        });
+        if (str.length > lastIndex) {
+            console.log("last part of string: " + parseGlobalRefs(str.slice(lastIndex, str.length), currentVarName, funcStr, regexps))
+        }
+        console.log("funcString is: " + funcStr)
+        return funcStr
         var func = new Function('options', 'Sqrl', funcString.replace(/\n/g, '\\n').replace(/\r/g, '\\r'))
         return func
     }
