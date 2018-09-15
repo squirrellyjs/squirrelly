27e289dbdc8a8522197644f104efc39608c7d67a
nebrelbug <nebrelbug@gmail.com>
2018-09-14 20:03:13 -0600

    Updated tests

--- test/test.js
+++ test/test.js
@@ -2,8 +2,95 @@
 var assert = require('assert')
 var Sqrl = require('../dist/squirrelly.min.js')
 
-describe('Compilation', function () {
-  it('Should return correct parsed value', function () {
-    assert.equal(Sqrl.Render(Sqrl.Compile('{{title}}'), {title: 'squirrelly is awesome'}), 'squirrelly is awesome')
+var simpleTemplate = `
+{{title}}
+`
+
+var bigTemplate = `
+Hi
+{{log("hey fellas")/}}
+{{htmlstuff}}
+{{foreach(options.obj)}}
+Reversed value: {{@this|reverse}}, Key: {{@key}}
+{{if(@key==="thirdchild")}}
+{{each(options.obj[@key])}}
+Salutations. Index:{{@index}} Old key: {{@../key}}
+{{/each}}
+{{/if}}
+{{/foreach}}{{ben()}}{{#gubler}}Hey{{#pineapple}}HI{{/ben}}
+{{tags(--,--)/}}
+Custom delimeters!
+--arr--
+`
+
+var bigTemplateResult = `
+Hi
+
+&lt;script&gt;alert(&#39;hey&#39;)&lt;/script&gt;&lt;p&gt;alert(&#39;hey&#39;)&lt;/p&gt;&lt;p&gt;alert(&#39;hey&#39;)&lt;/p&gt;&lt;p&gt;alert(&#39;hey&#39;)&lt;/p&gt;
+
+Reversed value: IH, Key: firstchild
+
+
+Reversed value: YEH, Key: secondchild
+
+
+Reversed value: 4,5,2,3,6,3, Key: thirdchild
+
+
+Salutations. Index:0 Old key: thirdchild
+
+Salutations. Index:1 Old key: thirdchild
+
+Salutations. Index:2 Old key: thirdchild
+
+Salutations. Index:3 Old key: thirdchild
+
+Salutations. Index:4 Old key: thirdchild
+
+Salutations. Index:5 Old key: thirdchild
+
+
+The content of gubler: Hey, the content of pineapple: HI
+
+Custom delimeters!
+Hey,&lt;p&gt;Malicious XSS&lt;/p&gt;,Hey,3,12
+`
+
+var simpleTemplateResult = `
+Squirrelly Tests
+`
+
+var data = {
+  htmlstuff: "<script>alert('hey')</script><p>alert('hey')</p><p>alert('hey')</p><p>alert('hey')</p>",
+  arr: ['Hey', '<p>Malicious XSS</p>', 'Hey', 3, 3 * 4],
+  obj: {
+    firstchild: 'HI',
+    secondchild: 'HEY',
+    thirdchild: [3, 6, 3, 2, 5, 4]
+  },
+  title: 'Squirrelly Tests'
+}
+
+Sqrl.defineFilter('reverse', function (str) {
+  var out = ''
+  for (var i = str.length - 1; i >= 0; i--) {
+    out += String(str).charAt(i)
+  }
+  return out || str
+})
+
+Sqrl.defineHelper('ben', function (args, content, blocks, options) {
+  return 'The content of gubler: ' + blocks.gubler() + ', the content of pineapple: ' + blocks.pineapple()
+})
+
+describe('Simple Compilation', function () {
+  it('Basic template returns correct value', function () {
+    assert.strictEqual(Sqrl.Render(simpleTemplate, data), simpleTemplateResult)
+  })
+})
+
+describe('Complex Compilation', function () {
+  it('Comprehensive template returns correct value', function () {
+    assert.strictEqual(Sqrl.Render(bigTemplate, data), bigTemplateResult)
   })
 })
