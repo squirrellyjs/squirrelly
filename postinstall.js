1d6af5901c9c83935b02f5ca449b55f9be5ef5f6
Ben Gubler <nebrelbug@gmail.com>
2019-02-14 17:07:38 -0700

    Changed the postinstall to have an ending newline, incremented version

--- postinstall.js
+++ postinstall.js
@@ -3,4 +3,6 @@ Read the Squirrelly docs at https://squirrelly.js.org.
 
 If you enjoy Squirrelly, consider donating at https://paypal.me/bengubler
 
-Have fun!`, '\x1b[0m')
+Have fun!
+
+`, '\x1b[0m')
