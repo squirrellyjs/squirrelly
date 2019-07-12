f775cc1a3ab5121c5c7f97b4bc8a65bd8e92813b
Ben Gubler <nebrelbug@gmail.com>
2019-07-12 13:43:38 -0600

    runtime.js

---
+++ src/runtime.js
@@ -0,0 +1,4 @@
+export { default as H } from './helpers'
+export { default as P } from './partials'
+export { defineFilter, defineHelper, definePartial, Render } from './utils'
+export { default as F, setDefaultFilters, autoEscaping } from './filters'
