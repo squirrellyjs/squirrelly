c5bae97c95fd13409e6a0388c4b036191d747b5c
Ben Gubler <nebrelbug@gmail.com>
2019-07-15 11:59:26 -0600

    Fixed bug with escaped single quotes (\\')

--- src/compile.js
+++ src/compile.js
@@ -1,29 +1,32 @@
-import {
-  regEx,
-  setup,
-  replaceParamHelpers
-} from './regexps'
+import { regEx, setup, replaceParamHelpers } from './regexps'
 import nativeHelpers from './nativeHelpers'
-import {
-  parseFiltered
-} from './filters'
+import { parseFiltered } from './filters'
 import P from './partials'
 
 function Compile (str) {
   var lastIndex = 0 // Because lastIndex can be complicated, and this way the minifier can minify more
-  var funcStr = '' // This will be called with Function() and returned
+  var funcStr = "var tR='';" // This will be called with Function() and returned
   var helperArray = [] // A list of all 'outstanding' helpers, or unclosed helpers
   var helperNumber = -1
   var helperAutoId = 0 // Squirrelly automatically generates an ID for helpers that don't have a custom ID
   var helperContainsBlocks = {} // If a helper contains any blocks, helperContainsBlocks[helperID] will be set to true
   var m
+
+  function addString (indx) {
+    if (lastIndex !== indx) {
+      funcStr +=
+        "tR+='" +
+        str
+          .slice(lastIndex, indx)
+          .replace(/\\/g, '\\\\')
+          .replace(/'/g, "\\'") +
+        "';"
+    }
+  }
+
   setup()
   while ((m = regEx.exec(str)) !== null) {
-    if (funcStr === '') {
-      funcStr += "var tR='" + str.slice(lastIndex, m.index).replace(/'/g, "\\'") + '\';'
-    } else if (lastIndex !== m.index) {
-      funcStr += "tR+='" + str.slice(lastIndex, m.index).replace(/'/g, "\\'") + '\';'
-    }
+    addString(m.index) // Add the string between the last tag (or start of file) and the current tag
     lastIndex = m[0].length + m.index
     if (m[1]) {
       // It's a global ref. p4 = filters
@@ -56,7 +59,14 @@ function Compile (str) {
         funcStr += nativeHelpers[m[5]].helperStart(params, id)
         lastIndex = regEx.lastIndex // the changeTags function sets lastIndex already
       } else {
-        funcStr += 'tR+=Sqrl.H.' + m[5] + '(' + params + ',function(hvals){var hvals' + id + "=hvals;var tR='';"
+        funcStr +=
+          'tR+=Sqrl.H.' +
+          m[5] +
+          '(' +
+          params +
+          ',function(hvals){var hvals' +
+          id +
+          "=hvals;var tR='';"
       }
     } else if (m[8]) {
       // It's a helper cTag.
@@ -64,7 +74,10 @@ function Compile (str) {
       if (mostRecentHelper && mostRecentHelper.name === m[8]) {
         helperNumber -= 1
         if (mostRecentHelper.native === true) {
-          funcStr += nativeHelpers[mostRecentHelper.name].helperEnd(mostRecentHelper.params, mostRecentHelper.id)
+          funcStr += nativeHelpers[mostRecentHelper.name].helperEnd(
+            mostRecentHelper.params,
+            mostRecentHelper.id
+          )
         } else {
           if (helperContainsBlocks[mostRecentHelper.id]) {
             funcStr += 'return tR}});'
@@ -84,14 +97,27 @@ function Compile (str) {
           funcStr += nativeH.blocks[m[9]](parent.id)
           lastIndex = regEx.lastIndex // Some native helpers set regEx.lastIndex
         } else {
-          console.warn("Native helper '%s' doesn't accept that block.", parent.name)
+          console.warn(
+            "Native helper '%s' doesn't accept that block.",
+            parent.name
+          )
         }
       } else {
         if (!helperContainsBlocks[parent.id]) {
-          funcStr += 'return tR},{' + m[9] + ':function(hvals){var hvals' + parent.id + "=hvals;var tR='';"
+          funcStr +=
+            'return tR},{' +
+            m[9] +
+            ':function(hvals){var hvals' +
+            parent.id +
+            "=hvals;var tR='';"
           helperContainsBlocks[parent.id] = true
         } else {
-          funcStr += 'return tR},' + m[9] + ':function(hvals){var hvals' + parent.id + "=hvals;var tR='';"
+          funcStr +=
+            'return tR},' +
+            m[9] +
+            ':function(hvals){var hvals' +
+            parent.id +
+            "=hvals;var tR='';"
         }
       }
     } else if (m[10]) {
@@ -99,15 +125,18 @@ function Compile (str) {
       var innerParams = m[11] || ''
       innerParams = replaceParamHelpers(innerParams)
       if (m[10] === 'include') {
-      // This code literally gets the template string up to the include self-closing helper,
-      // adds the content of the partial, and adds the template string after the include self-closing helper
+        // This code literally gets the template string up to the include self-closing helper,
+        // adds the content of the partial, and adds the template string after the include self-closing helper
         var preContent = str.slice(0, m.index)
         var endContent = str.slice(m.index + m[0].length)
         var partialParams = innerParams.replace(/'|"/g, '') // So people can write {{include(mypartial)/}} or {{include('mypartial')/}}
         var partialContent = P[partialParams]
         str = preContent + partialContent + endContent
         lastIndex = regEx.lastIndex = m.index
-      } else if (nativeHelpers.hasOwnProperty(m[10]) && nativeHelpers[m[10]].hasOwnProperty('selfClosing')) {
+      } else if (
+        nativeHelpers.hasOwnProperty(m[10]) &&
+        nativeHelpers[m[10]].hasOwnProperty('selfClosing')
+      ) {
         funcStr += nativeHelpers[m[10]].selfClosing(innerParams)
         lastIndex = regEx.lastIndex // changeTags sets regEx.lastIndex
       } else {
@@ -122,8 +151,9 @@ function Compile (str) {
     function helperRef (name, id, filters) {
       var prefix
       if (typeof id !== 'undefined') {
-        if (/(?:\.\.\/)+/g.test(id)) { // Test if the helper reference is prefixed with ../
-          prefix = helperArray[helperNumber - (id.length / 3) - 1].id
+        if (/(?:\.\.\/)+/g.test(id)) {
+          // Test if the helper reference is prefixed with ../
+          prefix = helperArray[helperNumber - id.length / 3 - 1].id
         } else {
           prefix = id.slice(0, -1)
         }
@@ -133,13 +163,13 @@ function Compile (str) {
     }
     /* eslint-enable no-inner-declarations */
   }
-  if (funcStr === '') {
-    funcStr += "var tR='" + str.slice(lastIndex, str.length).replace(/'/g, "\\'") + "';"
-  } else if (lastIndex !== str.length) {
-    funcStr += "tR+='" + str.slice(lastIndex, str.length).replace(/'/g, "\\'") + "';"
-  }
+  addString(str.length) // Add the string from the last tag-close to the end of the file, if there is one
   funcStr += 'return tR'
-  var func = new Function('options', 'Sqrl', funcStr.replace(/\n/g, '\\n').replace(/\r/g, '\\r')) //eslint-disable-line
+  var func = new Function( //eslint-disable-line
+    'options',
+    'Sqrl',
+    funcStr.replace(/\n/g, '\\n').replace(/\r/g, '\\r')
+  )
   return func
 }
 
