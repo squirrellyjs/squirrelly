36f8db1a2310230575ae295b365ce42dff5ad61b
vinothbabukm <vinoth.babu@cvshealth.com>
2020-05-30 14:51:56 -0400

    This condition will always return true since the function is always defined. Did you mean to call it instead? - Alrenate way of checking if the method exists in the prototype.

--- src/utils.ts
+++ src/utils.ts
@@ -83,7 +83,7 @@ function trimWS (
   if (leftTrim === '_' || leftTrim === 'slurp') {
     // console.log('trimming left' + leftTrim)
     // full slurp
-    if (String.prototype.trimLeft) {
+    if ('trimLeft' in String.prototype) {
       str = str.trimLeft()
     } else {
       str = str.replace(/^[\s\uFEFF\xA0]+/, '')
@@ -97,7 +97,7 @@ function trimWS (
   if (rightTrim === '_' || rightTrim === 'slurp') {
     // console.log('trimming right' + rightTrim)
     // full slurp
-    if (String.prototype.trimRight) {
+    if ('trimRight' in String.prototype) {
       str = str.trimRight()
     } else {
       str = str.replace(/[\s\uFEFF\xA0]+$/, '')
