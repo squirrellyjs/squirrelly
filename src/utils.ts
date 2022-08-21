725e23cc69a38ac0734e14bdbe5e1af80946d92c
Javiani <edudu_tata@hotmail.com>
2022-08-21 14:59:15 -0300

    Fixing issues with undefined variables

--- src/utils.ts
+++ src/utils.ts
@@ -23,7 +23,13 @@ try {
 export { asyncFunc }
 
 export const safeCompile = (variableName: string, returnStr: string) : string => {
-  return `(function(){ try{ return ${variableName} }catch(err) { return ${returnStr}} })()`
+  return `(function(){
+    try{
+      return ${variableName}? ${variableName}: ${returnStr}
+    }catch(err) {
+      return ${returnStr}
+    }
+  })()`
 }
 
 export function hasOwnProp (obj: object, prop: string) {
